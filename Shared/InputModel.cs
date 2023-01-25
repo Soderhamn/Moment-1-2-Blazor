71e6ed079d65239cbdb8f4c79ae156569f21e980
Marcus Andersson <marcus@apparant.se>
2023-01-25 09:50:32 +0100

    Konvertering klart

---
+++ Shared/InputModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Moment_1_2_Blazor_Wasm.Shared
+{
+	public class InputModel
+	{
+		public string? StrMetres { get; set; }
+
+		public string? StrFeet { get; set; }
+		public double Metres { get; set; }
+		public double Feet { get; set; }
+
+
+		//Konvertera strängen metres till double
+		public void ConvertToDouble()
+		{
+			this.Metres = Convert.ToDouble(this.StrMetres);
+		}
+
+		//Konvertera M till Feet
+		public string ConvertToFeet()
+		{
+			this.Feet = (this.Metres / 0.3048); //Räkna om M till Feet
+			this.Feet = Math.Round(Feet, 2); //Två decimaler
+			this.StrFeet = this.Feet.ToString(); //Konvertera till sträng
+
+			return this.StrFeet; //Returnera strängen
+
+        }
+	}
+}
---
+++ Shared/SecondModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Moment_1_2_Blazor_Wasm.Shared
+{
+	public class SecondModel
+	{
+		public int Kmh { get; set; }
+		public int Mph { get; set; }
+
+		//Konvertera Kilometer i timmen till miles per hour
+		public void ConvertKmhToMph()
+		{
+			double temp = this.Kmh / 1.609344; //Konvertera kmh till mph
+			this.Mph = Convert.ToInt32(temp); //Konvertera double till Int (Heltal)
+
+
+        }
+	}
+}
